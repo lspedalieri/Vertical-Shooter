e077ac22faf12fcb2e015ae6bd99410bf8821110
lspedalieri <leopoldo.spedalieri@gmail.com>
2023-12-19 00:11:02 +0100

    Lesson 11 - Menus, Game loop, Start scene, game over scene

---
+++ world.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+@onready var ship = $Ship
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	ship.tree_exiting.connect(func():
+		await get_tree().create_timer(1.0).timeout
+		get_tree().change_scene_to_file("res://menus/game_over.tscn")
+	)
+
